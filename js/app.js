f66b7df6cc77a56a085b30d214cd45eb9fac2598
mthwbrwn <mthwbrwn@gmail.com>
2018-06-05 15:37:41 -0700

    changes made to js newUser prompt/response works

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-var newJs = prompt('what\'s my name again?', 'guest');
-console.log (newJs);
+'use strict'
+
+var newUser = prompt('what\'s my name again?', 'guest');
+console.log ('user name is:', newUser);
+
+if (newUser.toLowerCase () === 'guest'){
+  alert('Guest is a boring name, but I respect your privacy!')
+} else  { 
+  alert('Welcome ' + newUser + '! Let\'s get to know me!')
+}
