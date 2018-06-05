ebacfadb43f15e91f67180e1f92e2a48a0b03fbe
mthwbrwn <mthwbrwn@gmail.com>
2018-06-05 16:42:15 -0700

    4 additional questions asked in js/app

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
-var moveOnYes = 'That\'s Correct! Let\'s Move On!!'
-var moveOnNo = 'that\s not quite right. Keep Going!'
+var moveOnRight = 'That\'s Correct! Let\'s Move On!!'
+var moveOnWrong = 'that\s not quite right. Keep Going!'
 var notYesNo = 'Hmmm, Try a Yes or No '
 var newUser = prompt('what\'s my name again?', 'guest');
 console.log ('user name is:', newUser);
@@ -11,14 +11,66 @@ if (newUser.toLowerCase () === 'guest'){
 } else { 
   alert('Welcome ' + newUser + '! Let\'s get to know me!');
 }
-
+// question 1
 var firstCode = prompt('Is this the first coding school Matthew has attended?');
-console.log (newUser + ' guessed', firstCode + 'to firstCode');
+console.log (newUser + ' guessed', firstCode + ' to firstCode');
 
 if (firstCode.toLowerCase() === 'yes'){
-  confirm( moveOnYes );
+  confirm( moveOnRight );
 } else if (firstCode.toLowerCase() === 'no' ) {
-  confirm (moveOnNo);
+  confirm (moveOnWrong);
 } else {
   alert(notYesNo);
 }
+
+// question 2 
+var doesCook = prompt('Does Matthew enjoy cooking?');
+console.log (newUser + ' guessed', doesCook + ' to doesCook');
+
+if (doesCook.toLowerCase() === 'yes'){
+  confirm( moveOnRight );
+} else if (doesCook.toLowerCase() === 'no') {
+  confirm (moveOnWrong);
+} else {
+  alert(notYesNo);
+}
+
+// question 3 
+var isSign = prompt('Wow. He seem so compatable with me. Is Matthew a Leo?');
+console.log (newUser + ' guessed', isSign + ' to isSign ');
+
+if (isSign.toLowerCase() === 'yes'){
+  confirm( moveOnWrong );
+} else if (isSign.toLowerCase() === 'no') {
+  confirm (moveOnRight);
+} else {
+  alert(notYesNo);
+}
+
+// question 4
+
+var fromHere = prompt('Is Matthew from Seattle?');
+console.log (newUser + ' guessed', fromHere + ' to fromHere ');
+
+if (fromHere.toLowerCase() === 'yes'){
+  confirm( moveOnRight );
+} else if (fromHere.toLowerCase() === 'no') {
+  confirm (moveOnWrong);
+} else {
+  alert(notYesNo);
+}
+  // question 5
+
+
+  var sportsBall = prompt('So you\'re from Seattle. Do Matthew watch the M\'s?');
+console.log (newUser + ' guessed', sportsBall + ' to sportsBall ');
+
+if (sportsBall.toLowerCase() === 'yes'){
+  confirm( moveOnWrong );
+} else if (sportsBall.toLowerCase() === 'no') {
+  confirm (moveOnRight);
+} else {
+  alert(notYesNo);
+}
+
+//  var endAlert= alert('Thank you for taking the time to get to know me');
